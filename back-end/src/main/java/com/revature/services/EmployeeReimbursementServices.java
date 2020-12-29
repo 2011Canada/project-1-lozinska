95c96abdb467db015fabc9e9e3cf45ab937b2271
Krystyna Lopez <klopez8@myseneca.ca>
2020-12-29 16:38:11 -0500

    Add service implementation

--- back-end/src/main/java/com/revature/services/EmployeeReimbursementServices.java
+++ back-end/src/main/java/com/revature/services/EmployeeReimbursementServices.java
@@ -6,7 +6,7 @@ import com.revature.models.Reimbursement;
 
 public interface EmployeeReimbursementServices {
 
-	public Reimbursement createReimbursement(Reimbursement r);
+	public Reimbursement createReimbursement(Reimbursement r, int authorID, int statusID, int typeID);
 	public Reimbursement updateStatus(Reimbursement r, int statusID);
 	public List<Reimbursement> findAllTickets();
 	public List<Reimbursement> findTicketByStatus(int statusID);
--- back-end/src/main/java/com/revature/services/EmployeeReimbursementServicesImplementation.java
+++ back-end/src/main/java/com/revature/services/EmployeeReimbursementServicesImplementation.java
@@ -1,5 +1,43 @@
 package com.revature.services;
 
-public class EmployeeReimbursementServicesImplementation {
+import java.util.List;
+
+import com.revature.models.Reimbursement;
+import com.revature.repositories.EmplReimbDAO;
+
+public class EmployeeReimbursementServicesImplementation implements EmployeeReimbursementServices{
+EmplReimbDAO ed;
+public EmployeeReimbursementServicesImplementation(EmplReimbDAO ed) {
+	this.ed=ed;
+}
+	@Override
+	public Reimbursement createReimbursement(Reimbursement r, int authorID, int typeID, int statusID) {
+		
+		return ed.createTicket(r, authorID, typeID, statusID);
+	}
+
+	@Override
+	public Reimbursement updateStatus(Reimbursement r, int statusID) {
+		// TODO Auto-generated method stub
+		return ed.updateStatus(r, statusID);
+	}
+
+	@Override
+	public List<Reimbursement> findAllTickets() {
+		// TODO Auto-generated method stub
+		return ed.findAll();
+	}
+
+	@Override
+	public List<Reimbursement> findTicketByStatus(int statusID) {
+		// TODO Auto-generated method stub
+		return ed.findAllByStatus(statusID);
+	}
+
+	@Override
+	public List<Reimbursement> findTicketByUser(int userID) {
+		// TODO Auto-generated method stub
+		return ed.findAllByUserID(userID);
+	}
 
 }
